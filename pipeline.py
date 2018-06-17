9f1453a1ca5911fb60b94be3bd7de71054507fb8
Maximilien Breughe <mbreughe@nvidia.com>
2018-06-16 19:58:32 -0500

    Correct RGB colors
    * Added transform to have the color channels in right order
    * Added more rectangles for the transformation
    
    NOTE: the perspective transform should actually be done 'after' gradient
    threshold!

--- pipeline.py
+++ pipeline.py
@@ -30,9 +30,9 @@ def getCalibrationPoints(fname):
 def test_before_after(img_before, img_after, ofname):
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
     f.tight_layout()
-    ax1.imshow(img_before)
+    ax1.imshow(cv2.cvtColor(img_before, cv2.COLOR_BGR2RGB))
     ax1.set_title('Original Image', fontsize=50)
-    ax2.imshow(img_after)
+    ax2.imshow(cv2.cvtColor(img_after, cv2.COLOR_BGR2RGB))
     ax2.set_title('Undistorted Image', fontsize=50)
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
     plt.savefig(ofname)
@@ -40,9 +40,16 @@ def test_before_after(img_before, img_after, ofname):
 if __name__ == "__main__":
     cal_dir = "camera_cal"
     cal_save = "cal_data.p"
-    
+
     transform_src = np.array([[250, 680], [535, 490], [752, 490], [1058, 680]], np.float32)
     transform_dst = np.array([[250, 680], [250, 490], [1058, 490], [1058, 680]], np.float32)
+    
+    #transform_src = np.array([[250, 680], [593, 450], [687, 450], [1058, 680]], np.float32)
+    #transform_dst = np.array([[250, 680], [250, 450], [1058, 450], [1058, 680]], np.float32)
+    
+    #transform_src = np.array([[250, 680], [624, 431], [655, 431], [1058, 680]], np.float32)
+    #transform_dst = np.array([[250, 680], [250, 431], [1058, 431], [1058, 680]], np.float32)
+    
     
     print("Getting calibration points")
     # Collect calibration points
@@ -72,11 +79,11 @@ if __name__ == "__main__":
     M = cv2.getPerspectiveTransform(transform_src, transform_dst)
     
     print("Testing")
-    fname = os.path.join("test_images", "straight_lines1.jpg")
+    fname = os.path.join("test_images", "straight_lines2.jpg")
     img = cv2.imread(fname)
     und = undistort(img, objectPoints, imagePoints)
     img_size = (und.shape[1], und.shape[0])
-    warped = cv2.warpPerspective(und, M, img_size)
+    warped = cv2.warpPerspective(und, M, img_size, flags=cv2.INTER_NEAREST)
     
     # Test out calibration
     test_before_after(img, warped, "test.jpg")
